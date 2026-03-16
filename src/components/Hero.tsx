6fc3f47763e7a5d5fbfac5c257d1896a817b832e
Anatolii Kretsu <tolyakretsu@gmail.com>
2026-03-16 17:10:34 +0200

    Quick fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
         src="https://images.unsplash.com/photo-1504674900247-0877df9cc836"
         className="w-full h-full object-cover"
       />
-      <div className="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent" />
+      <div className="absolute inset-0 bg-linear-to-t from-black/80 to-transparent" />
       <div className="absolute bottom-8 left-10">
         <h2 className="text-3xl font-bold text-white">Путешествуй через вкус</h2>
         <p className="text-gray-300 mt-1">10 блюд из самых популярных стран мира</p>
