fa85df1d7bc4b86b46613d39127b11cef32e1a58
svetlana-comur <svetlana.comur@isa.utm.md>
2026-03-20 20:08:51 +0200

    I changed the images on the main page

--- src/pages/Hotels.tsx
+++ src/pages/Hotels.tsx
@@ -6,7 +6,7 @@ export default function Hotels(_: PageProps) {
       <h1 className="text-3xl text-purple-400">Бутик-Отели</h1>
       <div className="grid md:grid-cols-2 gap-6">
         <img src="https://images.unsplash.com/photo-1551882547-ff43c63efe81" className="rounded-2xl" />
-        <img src="https://images.unsplash.com/photo-1566073771259-6a8506099945" className="rounded-2xl" />
+        <img src="https://t4.ftcdn.net/jpg/09/12/90/33/360_F_912903382_mq5bts1Xw74KGFzHrvKWbjHAzA3XbGl5.jpg" className="rounded-2xl" />
       </div>
       <p className="text-gray-300">Мы выбираем отели как искусство.</p>
     </div>
